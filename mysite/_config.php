d6958c3daed664b9d03a506919388024d0d7310a
Jack Marchant <jack@expert360.com>
2018-04-28 22:11:30 +1000

    force ssl

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,4 +1,10 @@
 <?php
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Kernel;
+
+if (Director::isLive()) {
+	Director::forceSSL();
+}
 
 //Heroku ClearDB support
 if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
@@ -9,7 +15,6 @@ if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
 	$databaseConfig['username'] = $parts['user'];
 	$databaseConfig['password'] = $parts['pass'];
 	$databaseConfig['database'] = trim($parts['path'], '/');
-	Security::setDefaultAdmin('admin', 'password');
 } else {
 	//Default SilverStripe environement support
 	require_once('conf/ConfigureFromEnv.php');
